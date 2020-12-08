f82e3b7b9b0d28d4628bf6aab090c78e0adf00fc
Will <doggy.huang@gmail.com>
2020-12-09 00:00:20 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericHostConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
